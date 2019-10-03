103847bd28c11f4f55206b69ff102448365f8623
DimejiAre <olardi2@gmail.com>
2019-10-03 13:08:01 +0100

    import additional components to test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Todos from './components/Todos';
 import Counter from './components/Counter';
+import Form from './components/Form';
+import Market from './components/Market';
 
 ReactDOM.render(
-  <Counter user='Pere' countLimit={5} />,
+  <>
+  <Counter user='Pere' countLimit={5}/>
+  <Todos user='Pereira' />
+  <Form user='Mariam' />
+  <Market user='Megan' />
+</>,
   document.getElementById('root'));
